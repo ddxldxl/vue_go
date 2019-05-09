865f12294fa872a5d2f47eb1617f44c6afd624d2
pxy <dengxiaoliang@yxj.org.cn>
2019-05-09 15:22:02 +0800

    vue开发结构

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require("path")
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin/' : '/'
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@',resolve('src'))
+      .set('_c',resolve('src/components'))
+  },
+  // 打包时不生产.map文件
+  productionSourceMap:false,
+
+  // 代理
+  devServer:{
+    proxy:"http://localhost:8080"
+  }
 }
